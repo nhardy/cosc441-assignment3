5a69458f9dcfd1b7dbadc87abf4221e579c6c88d
Nathan Hardy <nhardy@live.com.au>
2017-09-22 11:57:59 +1200

    Add parallel map

--- a3.erl
+++ a3.erl
@@ -12,8 +12,22 @@ a3(N, T) ->
 worker(P, L, U) ->
     P ! {self(), sumRange(L, U)}.
 
+% Parallel Map (pmap/2)
+% @see http://www.cs.otago.ac.nz/cosc441/2017-L10.pdf
+pmap(F, [X|Xs]) ->
+    S = self(),
+    P = spawn(fun () -> S ! {self(), F(X)} end),
+    Ys = pmap(F, Xs),
+    receive {P, Y} ->
+        [Y|Ys]
+    end;
+pmap(_, []) ->
+    [].
+
 % Sum of all integers N, where L <= N <= U
 sumRange(L, L) ->
     L;
 sumRange(L, U) ->
+    % Tail-call recursion allows the compiler
+    % to optimise recursion as iteration
     U + sumRange(L, U - 1).
